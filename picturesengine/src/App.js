9236d67bac20bcd461d345d5888d258249b6b552
muazam-cmd <muazamshahid7@gmail.com>
2023-01-31 16:23:58 +0400

    wrapping up an app, creating css files storing data from api call in a variable and using sync and await

--- picturesengine/src/App.js
+++ picturesengine/src/App.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import SearchBar from "./Components/SearchBar";
-import searchImages from './api'
+import searchImages from './api';
+import ImageList from './Components/ImageList';
 
 function App() {
-  const handleSubmit = (term) => {
-    searchImages(term);
+
+  const [images, setImage] = useState([]);
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term);
+
+    setImage(result);
   }
   return <div>
     <SearchBar onSubmit={handleSubmit} />
+    <ImageList images={images} />
   </div>
 }
 
